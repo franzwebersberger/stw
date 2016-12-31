e4aa84c6befe9cde3362e4a5ce3695b77d733244
Franz Webersberger <franzwebersberger@users.noreply.github.com>
2016-12-31 16:08:38 +0100

    cln: simplified StateMachine implementation

--- app/src/main/kotlin/com/fw/generic/api/statemachine/StateMachine.kt
+++ app/src/main/kotlin/com/fw/generic/api/statemachine/StateMachine.kt
@@ -3,18 +3,11 @@ package com.fw.generic.api.statemachine
 /**
  * Created by fw on 27.12.16.
  */
-class StateMachine<S, E, C> {
+class StateMachine<S, in E, in C>(var state: S, vararg ts: T<S, E, C>) {
 
-    data class T<out S, out E, C>(val state: S, val event: E, val nextState: S, val callback: (C) -> Unit = {})
+    data class T<out S, out E, in C>(val state: S, val event: E, val nextState: S, val callback: (C) -> Unit = {})
 
-    var state: S
-
-    private val transitions: Map<Pair<S, E>, T<S, E, C>>
-
-    constructor(state: S, vararg ts: T<S, E, C>) {
-        this.state = state
-        this.transitions = ts.associateBy({ Pair(it.state, it.event) }, { it })
-    }
+    private val transitions: Map<Pair<S, E>, T<S, E, C>> = ts.associateBy({ Pair(it.state, it.event) }, { it })
 
     fun handleEvent(event: E, context: C) = transitions[Pair(state, event)]?.let {
         state = it.nextState
